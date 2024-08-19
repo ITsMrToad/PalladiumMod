de35859261ce0ec547f5a67fa0366040b27dc58a
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2024-08-19 18:24:36 +0500

    Remake of block entity renderer freezer | 0.9.6

--- src/main/java/com/mr_toad/palladium/core/event/PCommonEvents.java
+++ src/main/java/com/mr_toad/palladium/core/event/PCommonEvents.java
@@ -4,7 +4,6 @@ import com.mr_toad.palladium.client.resource.PalladiumCacheReloadListener;
 import com.mr_toad.palladium.client.shader.ShaderCacheLoader;
 import com.mr_toad.palladium.core.Palladium;
 import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
-import net.minecraftforge.event.level.LevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -14,29 +13,5 @@ public class PCommonEvents {
     @SubscribeEvent
     public static void registerReloadListeners(RegisterClientReloadListenersEvent event) {
         event.registerReloadListener(PalladiumCacheReloadListener.LISTENER);
-    }
-
-    @Mod.EventBusSubscriber(modid = Palladium.MODID)
-    public static class NoBus {
-
-        @SubscribeEvent
-        public static void onLevelLoad(LevelEvent.Save event) {
-            BlockEntityRendererFreezerManager.clear();
-        }
-
-        @SubscribeEvent
-        public static void onLevelLoad(LevelEvent.Load event) {
-            BlockEntityRendererFreezerManager.LOAD = Palladium.checkConfigAndGetValueInt(cfg -> cfg.blockEntityReplaceRange) == 0;
-            if (Palladium.checkConfigAndGetValue(cfg -> cfg.enableBlockEntityReplace) && BlockEntityRendererFreezerManager.LOAD) {
-                BlockEntityRendererFreezerManager.load((Level) event.getLevel(), ((LevelRendererAccessor) Minecraft.getInstance().levelRenderer).getGlobalBlockEntities());
-            }
-        }
-
-        @SubscribeEvent
-        public static void onTickLevel(TickEvent.LevelTickEvent event) {
-            if (Palladium.checkConfigAndGetValue(cfg -> cfg.enableBlockEntityReplace) && !BlockEntityRendererFreezerManager.LOAD) {
-                BlockEntityRendererFreezerManager.tick(event.level, ((LevelRendererAccessor) Minecraft.getInstance().levelRenderer).getGlobalBlockEntities());
-            }
-        }
     }
 }
