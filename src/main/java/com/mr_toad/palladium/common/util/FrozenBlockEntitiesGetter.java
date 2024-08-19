e2c5e09500c46ee63de07f49be894660e8b65fd7
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2024-08-19 18:27:29 +0500

    Remake of block entity renderer freezer | 0.9.6

---
+++ src/main/java/com/mr_toad/palladium/common/util/FrozenBlockEntitiesGetter.java
@@ -0,0 +1,7 @@
+package com.mr_toad.palladium.common.util;
+
+import net.minecraft.world.level.block.entity.BlockEntity;
+
+public interface FrozenBlockEntitiesGetter {
+    ImmutableArrayList<BlockEntity> palladium$get();
+}
