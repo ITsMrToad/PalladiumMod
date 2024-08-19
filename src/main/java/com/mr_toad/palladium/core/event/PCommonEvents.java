316a714c0f6e4f0f111a439ba6c5ff990b7ec42a
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2024-08-19 16:03:25 +0500

    Update PCommonEvents | 0.9.5

--- src/main/java/com/mr_toad/palladium/core/event/PCommonEvents.java
+++ src/main/java/com/mr_toad/palladium/core/event/PCommonEvents.java
@@ -16,4 +16,27 @@ public class PCommonEvents {
         event.registerReloadListener(PalladiumCacheReloadListener.LISTENER);
     }
 
+    @Mod.EventBusSubscriber(modid = Palladium.MODID)
+    public static class NoBus {
+
+        @SubscribeEvent
+        public static void onLevelLoad(LevelEvent.Save event) {
+            BlockEntityRendererFreezerManager.clear();
+        }
+
+        @SubscribeEvent
+        public static void onLevelLoad(LevelEvent.Load event) {
+            BlockEntityRendererFreezerManager.LOAD = Palladium.checkConfigAndGetValueInt(cfg -> cfg.blockEntityReplaceRange) == 0;
+            if (Palladium.checkConfigAndGetValue(cfg -> cfg.enableBlockEntityReplace) && BlockEntityRendererFreezerManager.LOAD) {
+                BlockEntityRendererFreezerManager.load((Level) event.getLevel(), ((LevelRendererAccessor) Minecraft.getInstance().levelRenderer).getGlobalBlockEntities());
+            }
+        }
+
+        @SubscribeEvent
+        public static void onTickLevel(TickEvent.LevelTickEvent event) {
+            if (Palladium.checkConfigAndGetValue(cfg -> cfg.enableBlockEntityReplace) && !BlockEntityRendererFreezerManager.LOAD) {
+                BlockEntityRendererFreezerManager.tick(event.level, ((LevelRendererAccessor) Minecraft.getInstance().levelRenderer).getGlobalBlockEntities());
+            }
+        }
+    }
 }
