8e2dd6759ddfec1c6e629beb007d671e408227d0
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2024-08-20 12:43:59 +0500

    Delete block entity replacer system, uncorrect work | 0.9.8

--- src/main/java/com/mr_toad/palladium/common/util/FrozenBlockEntitiesGetter.java
+++
@@ -1,7 +0,0 @@
-package com.mr_toad.palladium.common.util;
-
-import net.minecraft.world.level.block.entity.BlockEntity;
-
-public interface FrozenBlockEntitiesGetter {
-    ImmutableArrayList<BlockEntity> palladium$get();
-}
